b90a4546c3eeecb7ff55bbe3f86ffd9c994938ee
Kareem El-Giushy <kareem.64123@gmail.com>
2024-05-25 06:36:03 +0300

    Finish Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 
 import { ProductsModule } from './products/products.module';
 import { FilterPipe } from './filter.pipe';
+import { CopyrightDirective } from './copyright.directive';
+import { NumericDirective } from './numeric.directive';
+import { PermissionDirective } from './permission.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CopyrightDirective,
+    NumericDirective,
+    PermissionDirective
   ],
   imports: [
     BrowserModule,
